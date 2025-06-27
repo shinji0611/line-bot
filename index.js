a8ea155644772c364e7b8c86bc923072ef4b0239
shinji0611 <yamadaz0611@gmail.com>
2025-06-27 19:24:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require('express');
+const app = express();
 const line = require('@line/bot-sdk');
 
 const config = {
   channelAccessToken: '【fHJIgvHg1ZtHgD9RTvmyLvdQb8U9e+06Pj24b4m7YVR8Vn1fepH1kumqyeCRW/hxzAp922h29Fjn/N7ePyEPmJJ2qhFlAf8e/qfXpueecT6X4VuTJPJC6/x4sGujWyIJJHSVbY4tNlLjnhSp621q/AdB04t89/1O/w1cDnyilFU=】',
-  channelSecret: '【27460bb1fba83b4ebe6fd0376a203663】'
+  channelSecret: '【27460bb1fba83b4ebe6fd0376a203663】',
 };
 
-const app = express();
 const client = new line.Client(config);
+
+app.use(express.json());
 
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then(result => res.json(result))
-    .catch(err => {
-      console.error(err);
-      res.status(500).end();
-    });
+    .then((result) => res.json(result));
 });
 
 function handleEvent(event) {
@@ -24,13 +22,11 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
+  // ここが返信内容
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: あなたは「${event.message.text}」と言いましたね！
+    text: `あなたは「${event.message.text}」と送りましたね！`,
   });
 }
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+app.listen(10000);

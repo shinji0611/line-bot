ffa3fdd7ba77bcb16edca41615a95eab7ec93a7c
shinji0611 <yamadaz0611@gmail.com>
2025-06-27 20:35:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const app = express();
+const express = require('express');
 const line = require('@line/bot-sdk');
+const app = express();
 
 const config = {
   channelAccessToken: 'fHJIgvHg1ZtHgD9RTvmyLvdQb8U9e+06Pj24b4m7YVR8Vn1fepH1kumqyeCRW/hxzAp922h29Fjn/N7ePyEPmJJ2qhFlAf8e/qfXpueecT6X4VuTJPJC6/x4sGujWyIJJHSVbY4tNlLjnhSp621q/AdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '27460bb1fba83b4ebe6fd0376a203663',
+  channelSecret: '27460bb1fba83b4ebe6fd0376a203663'
 };
 
 const client = new line.Client(config);
@@ -11,7 +12,8 @@ const client = new line.Client(config);
 app.use(express.json());
 
 app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
+  Promise
+    .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
@@ -20,12 +22,13 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 });
 
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message?.type !== 'text') {
+  if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: `あなたは「${event.message.text}」と送りましたね！`,
+    text: あなたは「${event.message.text}」と送りましたね！
   });
 }
 

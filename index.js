4e33d03cd582830ba0e1b154991aa9e9d2c91020
shinji0611 <yamadaz0611@gmail.com>
2025-06-28 18:19:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const line = require('@line/bot-sdk');
 const { OpenAI } = require('openai');
 
 const app = express();
-app.use(express.json());
+app.use(express.text({ type: "*/*" }));
 
 // LINE設定
 const config = {
